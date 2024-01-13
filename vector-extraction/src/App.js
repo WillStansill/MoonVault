fc78ab891f718da43f5d85d1d0e7bd64fd81ba76
WIll Stansill <willstansill@gmail.com>
2024-01-13 11:52:31 -0500

    Next Commit

---
+++ vector-extraction/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Connect Your Wallet</h1>
+      <button>Download</button>
+    </div>
+  );
+}
+
+export default App;
